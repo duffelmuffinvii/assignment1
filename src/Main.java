51eef941373ff9113b639b1a582c4d695d2a24dc
duffelmuffinvii <slkbrb@gmail.com>
2022-01-26 16:25:40 -0500

    reads in file as Nodes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,12 +12,28 @@ public class Main {
         try {
             File file = new File("maps/assignment 1, sample board.txt");
             Scanner scanner = new Scanner(file);
+            int y = 0;
             while (scanner.hasNextLine()) {
-
+                String line = scanner.nextLine();
+                int x = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    char curr = line.charAt(i);
+                    if (Character.isDigit(curr)) {
+                        map.add(new Node(x, y, Integer.parseInt(String.valueOf(curr))));
+                        x++;
+                    }
+                    if (curr == 'S') {x++;}
+                    if (curr == 'G') {x++;}
+                }
+                y++;
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+
+        for (Node n : map) {
+            System.out.println(n.x + ", " + n.y + ": " + n.cmplxty);
         }
 
     }
