1cdacfb52268e43ac3492ccdf36ff024a2538bd9
huynh <duybaohuynh1312@gmail.com>
2022-01-27 20:41:43 -0500

    Testing Main and Node

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,25 +2,116 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+//
+//        ArrayList<Node> map = new ArrayList<Node>();
+//
+//        try {
+//            File file = new File("maps/assignment 1, sample board.txt");
+//            Scanner scanner = new Scanner(file);
+//            while (scanner.hasNextLine()) {
+//
+//            }
+//            scanner.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
-        ArrayList<Node> map = new ArrayList<Node>();
+        Node head = new Node(3);
+        head.g = 0;
 
-        try {
-            File file = new File("maps/assignment 1, sample board.txt");
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
+        Node n1 = new Node(2);
+        Node n2 = new Node(2);
+        Node n3 = new Node(2);
 
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        head.addBranch(1, n1);
+        head.addBranch(5, n2);
+        head.addBranch(2, n3);
+        n3.addBranch(1, n2);
+
+        Node n4 = new Node(1);
+        Node n5 = new Node(1);
+        Node target = new Node(0);
+
+        n1.addBranch(7, n4);
+        n2.addBranch(4, n5);
+        n3.addBranch(6, n4);
+
+        n4.addBranch(3, target);
+        n5.addBranch(1, n4);
+        n5.addBranch(3, target);
+
+        Node res = aStar(head, target);
+        printPath(res);
 
     }
+
+    public static Node aStar(Node start, Node target){
+        PriorityQueue<Node> closedList = new PriorityQueue<>();
+        PriorityQueue<Node> openList = new PriorityQueue<>();
+
+        start.f = start.g + start.calculateHeuristic(target);
+        openList.add(start);
+
+        while(!openList.isEmpty()){
+            Node n = openList.peek();
+            if(n == target){
+                return n;
+            }
+
+            for(Node.Edge edge : n.neighbors){
+                Node m = edge.node;
+                double totalWeight = n.g + edge.weight;
+
+                if(!openList.contains(m) && !closedList.contains(m)){
+                    m.parent = n;
+                    m.g = totalWeight;
+                    m.f = m.g + m.calculateHeuristic(target);
+                    openList.add(m);
+                } else {
+                    if(totalWeight < m.g){
+                        m.parent = n;
+                        m.g = totalWeight;
+                        m.f = m.g + m.calculateHeuristic(target);
+
+                        if(closedList.contains(m)){
+                            closedList.remove(m);
+                            openList.add(m);
+                        }
+                    }
+                }
+            }
+
+            openList.remove(n);
+            closedList.add(n);
+        }
+        return null;
+    }
+
+    public static void printPath(Node target){
+        Node n = target;
+
+        if(n==null)
+            return;
+
+        List<Integer> ids = new ArrayList<>();
+
+        while(n.parent != null){
+            ids.add(n.id);
+            n = n.parent;
+        }
+        ids.add(n.id);
+        Collections.reverse(ids);
+
+        for(int id : ids){
+            System.out.print(id + " ");
+        }
+        System.out.println("");
+    }
 }
+
+
